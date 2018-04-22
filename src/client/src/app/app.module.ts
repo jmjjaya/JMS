a6b089f12e8bd8fdfdbee94b1899db6f385b47b2
jmjjaya <efacjeewan@gmail.com>
2018-04-22 17:02:49 -0500

    created modules core and shared and changed the prefix to jmsapp

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
