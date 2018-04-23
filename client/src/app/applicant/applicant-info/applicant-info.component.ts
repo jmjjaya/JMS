f49439a4ef18a24027daba4829434dfa69b8a2d5
jmjjaya <efacjeewan@gmail.com>
2018-04-23 12:03:12 -0500

    Add Applicant module

---
+++ client/src/app/applicant/applicant-info/applicant-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jmsapp-applicant-info',
+  templateUrl: './applicant-info.component.html',
+  styleUrls: ['./applicant-info.component.css']
+})
+export class ApplicantInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
