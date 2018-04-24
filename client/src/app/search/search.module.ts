d4abc52821f01d412e0cee9890df7d34727a0de3
jhanMum <jhan@mum.edu>
2018-04-24 17:22:54 -0500

    Mofify search dashboard

--- client/src/app/search/search.module.ts
+++ client/src/app/search/search.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search/search.component';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   imports: [
     CommonModule,
-    AccordionModule.forRoot()
+    AccordionModule.forRoot(),
+    NgxDatatableModule,
+    BrowserModule
   ],
   declarations: [SearchComponent],
   exports: [SearchComponent]
--- client/src/app/search/search/search.component.ts
+++ client/src/app/search/search/search.component.ts
@@ -1,27 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'jmsapp-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
   jobs = JOBS;
 
-  constructor() { }
+  @ViewChild('myTable') table: any;
 
-  ngOnInit() {
+  rows: any[] = [];
+  expanded: any = {};
+  timeout: any;
+
+  constructor() {
+    // this.fetch((data) => {
+    //   this.rows = data;
+    // });
+    this.rows = this.jobs;
   }
 
-  expandJob(event) {
-    // console.log(this.jobsTr);
-    let job_id = event.currentTarget.id;
-    let job_detail_element = event.srcElement.parentElement.nextElementSibling;
-    job_detail_element.style.display = job_detail_element.style.display == 'block' ? 'none' : 'block';
-
-    console.log(job_detail_element);
-    console.log(job_detail_element.style.display);
+  onPage(event) {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      console.log('paged!', event);
+    }, 100);
   }
+
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/100k.json`);
+
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
+  }
+
+  toggleExpandRow(row) {
+    console.log('Toggled Expand Row!', row);
+    this.table.rowDetail.toggleExpandRow(row);
+  }
+
+  onDetailToggle(event) {
+    console.log('Detail Toggled', event);
+  }
+
+
+  // expandJob(event) {
+  //   // console.log(this.jobsTr);
+  //   let job_id = event.currentTarget.id;
+  //   let job_detail_element = event.srcElement.parentElement.nextElementSibling;
+  //   job_detail_element.style.display = job_detail_element.style.display == 'block' ? 'none' : 'block';
+
+  //   console.log(job_detail_element);
+  //   console.log(job_detail_element.style.display);
+  // }
 
   applyJob(event) {
     alert("Go to Register or User profile page.");
@@ -39,6 +77,22 @@ export interface JobPosition {
 }
 
 const JOBS: JobPosition[] = [{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
-  { id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },
-  ];
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" },{ id: 1, title: "Data Engineering", company: "Citadel", tags: "Python R" },
+{ id: 2, title: "Data Scientist", company: "BOA", tags: "Matlab Hadoop" }
+];
 
